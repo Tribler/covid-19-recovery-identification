9114bc79d402b070d3a12220d0b0c7e1659689f0
Lesley Verdoner <L.franschman@student.tudelft.nl>
2020-05-17 22:57:14 +0200

    editing settingsscreen

--- SSI-App/screens/SettingsScreen.tsx
+++ SSI-App/screens/SettingsScreen.tsx
@@ -9,11 +9,14 @@ const SettingsScreen: React.FC = () => {
     
     return(
         <View style={styles.container}>
+         
             <h1 style={{fontWeight: "bold", verticalAlign:'top', fontSize:60, fontFamily:"Sans-serif"}}>Settings</h1>
             <Text style={styles.setting}>Push Notifications <Text style={styles.option}><Text onPress={()=> console.log("enabled")}>Enabled</Text> / <Text onPress={()=> console.log("disabled")}>Disabled</Text></Text></Text>
             <Text style={styles.setting}>Theme <Text style={styles.option1}><Text onPress={()=> console.log("light")}>Light</Text> / <Text onPress={()=> console.log("dark")}>Dark</Text> / <Text onPress={()=> console.log("corona")}>Corona</Text></Text><br/><br/><br/><br/><br/><br/><br/><br/></Text>
+          
             <Text style={styles.settingred} onPress={()=> console.log("delete cert")}>Delete a Certificate</Text>
             <Text style={styles.settingred} onPress={()=> console.log("delete acc")}>Delete your Account</Text>
+            
             <DrawerButton></DrawerButton>
         </View>
     )
@@ -24,16 +27,19 @@ const styles = StyleSheet.create({
       flex: 1,
       backgroundColor: '#fff',
       alignItems: 'center',
+      
     },
+  
     setting: {
         position: "relative",
-        top:50,
+        top:150,
         right:80,
         textAlign:"left",
         paddingVertical: 8,
         color: "#20232a",
         fontSize: 20,
-        fontFamily: "Sans-serif"
+        fontFamily: "Sans-serif",
+        
     },
     option: {
         position: "relative",
@@ -55,7 +61,7 @@ const styles = StyleSheet.create({
     },
     settingred: {
         position: "relative",
-        top:50,
+        top:200,
         right:161,
         textAlign:"left",
         paddingVertical: 8,
