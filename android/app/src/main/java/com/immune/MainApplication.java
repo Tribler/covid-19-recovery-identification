efe4a5631dce705e6b72be6d5e432f7031a570f7
kkostadinov <k.k.kostadinov@student.tudelft.nl>
2020-05-18 09:53:36 +0200

    Removed one unused import in MainApplication and formatted settings.gradle.

--- android/app/src/main/java/com/immune/MainApplication.java
+++ android/app/src/main/java/com/immune/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -51,9 +50,6 @@ public class MainApplication extends Application implements ReactApplication {
   /**
    * Loads Flipper in React Native templates. Call this in the onCreate method with something like
    * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-   *
-   * @param context
-   * @param reactInstanceManager
    */
   private static void initializeFlipper(
       Context context, ReactInstanceManager reactInstanceManager) {
