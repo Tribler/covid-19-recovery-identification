9e12de36145a21227ec82bff0867d3578afec1c7
Lesley Verdoner <L.franschman@student.tudelft.nl>
2020-05-19 02:41:41 +0200

    setting background immage and edit loginscreen

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View,TextInput } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View,TextInput } from 'react-native';
 import { Input , Image, } from 'react-native-elements';
 import PasswordInputText from 'react-native-hide-show-password-input';
  
@@ -11,33 +11,35 @@ const LoginScreen: React.FC = () => {
        
         <View style={styles.container}>
           
-        <Image source={require("../assets/logo.png")} />
-            <Text style={{ fontWeight: "bold", color: "#1d5" }}> sign in as patient</Text>
-         
-            <TextInput
-style={{height: 45,width: "95%",borderColor: "gray",borderWidth: 2}}
-// Adding hint in TextInput using Placeholder option.
-placeholder=" Enter Your Password"          
-// Making the Under line Transparent.
-underlineColorAndroid="transparent"
-// Making the Text Input Text Hidden.
-secureTextEntry={true}
-/>
+        {/* <Image source={require("../assets/logo.png")} /> */}
+           
+         <ImageBackground  
+            style={styles.im}
+            source={require('../assets/background.jpeg')}
+        ></ImageBackground>
+
+<Text style={{ fontWeight: "bold", color: "#1d5",fontSize:20 }}> sign in as patient</Text>
+         <Text>{"\n"}</Text>
+        <TextInput
+        style={{height: 45,width: "95%",borderColor: "gray",borderWidth: 2}}
+        placeholder=" Enter Your Password"          
+        underlineColorAndroid="transparent"
+        secureTextEntry={true}
+        />
             
            
-            <Input type="submit" value="login" />
-            <Text style={{ fontWeight: "bold", color: "#1d5" }}> sign in as health expert</Text>
            
+            <Input type="submit" value="login" />
+            <Text>{"\n"}</Text>
+            <Text style={{ fontWeight: "bold", color: "#1d5", fontSize:20 }}> sign in as health expert</Text>
+            <Text>{"\n"}</Text>
               
-            <TextInput
-style={{height: 45,width: "95%",borderColor: "gray",borderWidth: 2}}
-// Adding hint in TextInput using Placeholder option.
-placeholder=" Enter Your Password"          
-// Making the Under line Transparent.
-underlineColorAndroid="transparent"
-// Making the Text Input Text Hidden.
-secureTextEntry={true}
-/>
+      <TextInput
+        style={{height: 45,width: "95%",borderColor: "gray",borderWidth: 2}}
+        placeholder=" Enter Your Password"          
+        underlineColorAndroid="transparent"
+        secureTextEntry={true}
+        />
             
                 
                
@@ -45,7 +47,7 @@ secureTextEntry={true}
             <Input type="submit" value="login" />
             {
             }
-            
+           
         </View>
     )
 }
@@ -55,14 +57,27 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         //TODO This style configuration causes an error.
-        // backgroundImage: "url(" + require("../assets/background.jpeg") + ")",
+        //  ImageBackground: "url(" + require("../assets/background.jpeg") + ")",
         // backgroundPosition: "center",
-        // backgroundSize: "cover",
+       
         // backgroundRepeat: "no-repeat",
         alignItems: 'center',
         padding: 24,
         justifyContent: 'center',
-    }
+        bottom: 250
+      
+    },
+    im:{
+        
+       width: "110%",
+       height: "110%",
+       flexDirection: "column",
+       resizeMode: "cover",
+       top: 450,
+       right:30
+    },
+    
+    
 });
 
 export default LoginScreen
