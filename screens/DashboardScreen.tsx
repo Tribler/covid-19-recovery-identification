b9238ee1dd28c83516cff23a5f08e42043e0e4b5
Lesley Verdoner <L.franschman@student.tudelft.nl>
2020-05-20 16:36:44 +0200

    adding badge example

--- screens/DashboardScreen.tsx
+++ screens/DashboardScreen.tsx
@@ -7,12 +7,26 @@ const Dashboard: React.FC = () => {
     return (
         <View style={styles.light}>
             <Text style={styles.lighttext}>My Dashboard</Text>
-            <View style= {styles.certificate}>
+            <Text style ={styles.instructions} >you can find your earned badges below</Text>
+        <View style={styles.badges}>
+    
+            <View style={styles.twelvePointBurst}>
+        <View style={styles.twelvePointBurstMain} />
+        <View style={styles.twelvePointBurst30} />
+        <View style={styles.twelvePointBurst60} />
+        <Text style ={styles.badgeText}>Immunity</Text>
+
+      </View>   
+      
+      </View> 
+      <View style={styles.rectangle} >  
+      </View>
+            {/* <View style= {styles.certificate}>
                 <Text> Acquired proofs</Text>
                 <Text>{"\n"}</Text>
                 <Text> AIDS immunity</Text>
                 <Text> COVID-19 immunity</Text>
-            </View>
+            </View> */}
         <DrawerButton></DrawerButton>
         </View>
     )
@@ -59,6 +73,58 @@ const styles = StyleSheet.create({
         fontSize: 30,
         fontWeight: "bold",
         fontFamily: "Sans-serif"
+    },
+    twelvePointBurstMain: {
+        width: 100,
+        height: 100,
+        backgroundColor: "#74d14c",
+        top:200,
+        right:100,
+        borderColor:"black"
+      },
+      twelvePointBurst30: {
+        width: 100, 
+        height: 100,
+        position: 'absolute',
+        backgroundColor: '#74d14c',
+        borderColor:"black",
+        top: 200,
+        right: 100,
+        transform: [
+          {rotate: '30deg'}
+        ],
+        
+      },
+      twelvePointBurst60: {
+        width: 100, 
+        height: 100,
+        position: 'absolute',
+        backgroundColor: '#74d14c',
+        top: 200,
+        right: 100,
+        borderColor:"black",
+        transform: [
+          {rotate: '60deg'}
+        ]
+      },
+      badgeText:{
+        top:140,
+        right:80,
+        fontWeight:"bold",
+        
+        
+
+      },
+      instructions:{
+        top:80
+      },
+      rectangle: {
+        width: 150,
+        height: 200,
+        backgroundColor: 'grey',
+        opacity: 0.1,
+        top:30,  
+        right: 100
     }
 });
 
