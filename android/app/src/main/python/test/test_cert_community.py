59e2c696df647e23efb5d043026b4c911f413a64
s.cirikka <s.cirikka@student.tudelft.nl>
2020-05-29 01:03:03 +0200

    initial start of python unit tests

---
+++ android/app/src/main/python/test/test_cert_community.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+
+class TestCertCommunity(TestCase):
+    def test_send_certificate(self):
+        self.fail()
+
+    def test_on_certificate(self):
+        self.fail()
+
+    def test_read_certificates_file(self):
+        self.fail()
+
+    def test_write_certificates_file(self):
+        self.fail()
---
+++ android/app/src/main/python/test/test_certificate_endpoint.py
@@ -0,0 +1,102 @@
+from asyncio import sleep
+from base64 import b64encode
+from sys import modules
+from certificate_endpoint import CertificateEndpoint
+from cert_community import CertCommunity
+from pyipv8.ipv8.attestation.identity.community import IdentityCommunity
+from pyipv8.ipv8.attestation.wallet.community import AttestationCommunity
+from pyipv8.ipv8.test.REST.attestationendpoint.test_attestation_endpoint import TestAttestationEndpoint
+from pyipv8.ipv8.test.REST.rest_base import RESTTestBase, partial_cls
+
+root = modules["pyipv8.ipv8.REST.root_endpoint"]
+root.AttestationEndpoint = CertificateEndpoint
+modules["pyipv8.ipv8.REST.rest_manager"].RootEndpoint = root.RootEndpoint
+
+
+class TestCertificateEndpoint(RESTTestBase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.test_attestation_endpoint = TestAttestationEndpoint()
+
+    async def setUp(self):
+        super(TestCertificateEndpoint, self).setUp()
+        await self.initialize([partial_cls(AttestationCommunity, working_directory=':memory:'),
+                               partial_cls(IdentityCommunity, working_directory=':memory:'),
+                               partial_cls(CertCommunity, working_directory='resource')], 2)
+        open('resource/certificates.txt', 'w').close()
+
+    async def tearDown(self):
+        open('resource/certificates.txt', 'w').close()
+
+    async def make_id(self, node):
+        """
+        Forward a request for return of ID of a peer.
+        """
+        return await self.make_request(node, 'attestation/certificate/id', 'GET', {})
+
+    async def make_outstanding_certificates(self, node):
+        """
+        Forward a request to fetch the outstanding certificates for a peer.
+        """
+        return await self.make_request(node, 'attestation/certificate/recent', 'GET', {})
+
+    async def make_certificate(self, node, certificate_id, mid):
+        """
+        Forward a request to make a certificate.
+        """
+        return await self.make_request(node, 'attestation/certificate', 'POST', {'type': 'send',
+                                                                                 'certificate_id': certificate_id,
+                                                                                 'mid': mid})
+
+    async def create_certificate_request(self, node, certificate_id):
+        """
+        Helper method for test_post_certificate and test_get_certificate.
+        """
+        peer_list = await self.test_attestation_endpoint.wait_for_peers(node)
+        for mid in peer_list:
+            await self.make_certificate(node, certificate_id, mid)
+
+    async def wait_for_outstanding_certificates(self, node):
+        """
+        Wait until this peer receives a non-empty list of outstanding certificate requests.
+        """
+        outstanding_requests = await self.make_outstanding_certificates(node)
+        while not outstanding_requests:
+            await sleep(.1)
+            outstanding_requests = await self.make_outstanding_certificates(node)
+        return [(x[0], x[1]) for x in outstanding_requests]
+
+    async def test_get_id(self):
+        """
+        Test the (GET : id) request type.
+        """
+        result = await self.make_id(self.nodes[0])
+        self.assertTrue(result["id"] == b64encode(self.nodes[0].my_peer.mid).decode('utf-8'),
+                        "The returned peer ID is not equal to your own peer ID.")
+
+    async def test_get_certificate(self):
+        """
+        Test the (GET: recent) request type.
+        """
+        await self.introduce_nodes()
+        await self.create_certificate_request(self.nodes[1], '1')
+
+        result = await self.wait_for_outstanding_certificates(self.nodes[0])
+        mid = b64encode(self.nodes[1].my_peer.mid).decode('utf-8')
+        self.assertEqual(result[0][0], mid)
+        self.assertEqual(result[0][1], 'cv19-i')
+
+    async def test_post_certificate(self):
+        """
+        Test the (POST: send) request type.
+        """
+        outstanding_certificates = await self.make_outstanding_certificates(self.nodes[0])
+        self.assertEqual(outstanding_certificates, [], "Should be no outstanding certificates")
+
+        await self.introduce_nodes()
+        await self.create_certificate_request(self.nodes[1], '1')
+
+        outstanding_certificates = await self.make_outstanding_certificates(self.nodes[0])
+        self.assertEqual(outstanding_certificates, [[b64encode(self.nodes[1].my_peer.mid).decode('utf-8'), 'cv19-i']],
+                         "List of outstanding certificates should not be empty")
