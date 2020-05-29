858759035680df808307bed98e9a4cfdb9c72680
s.cirikka <s.cirikka@student.tudelft.nl>
2020-05-29 21:31:28 +0200

    add tests for cert_community

--- android/app/src/main/python/test/test_cert_community.py
+++ android/app/src/main/python/test/test_cert_community.py
@@ -1,15 +1,36 @@
-from unittest import TestCase
+import json
+import os
+
+from cert_community import CertCommunity
+from ipv8.test.base import TestBase
+from ipv8.test.mocking.ipv8 import MockIPv8
 
 
-class TestCertCommunity(TestCase):
-    def test_send_certificate(self):
-        self.fail()
+class TestCertCommunity(TestBase):
 
-    def test_on_certificate(self):
-        self.fail()
+    def setUp(self):
+        super(TestCertCommunity, self).setUp()
+
+    def tearDown(self):
+        os.remove('resource/certificates.txt')
+
+    def create_node(self):
+        return MockIPv8(u"curve25519", CertCommunity, working_directory=u"resource")
 
     def test_read_certificates_file(self):
-        self.fail()
+        self.assertEqual(os.path.exists('resource/certificates.txt'), False, "Should not be a file here yet")
+        self.initialize(CertCommunity, 2)
+        self.create_node()
+        self.assertEqual(os.path.exists('resource/certificates.txt'), True, "File should still be there")
 
-    def test_write_certificates_file(self):
-        self.fail()
+    def test_read_certificates_file_already_there(self):
+        self.assertEqual(os.path.exists('resource/certificates.txt'), False, "Should not be a file here yet")
+        f = open('resource/certificates.txt', 'a')
+        f.write(json.dumps({
+            "peer1": "cv19"
+        }))
+        f.close()
+        self.initialize(CertCommunity, 2)
+        node = self.create_node()
+        self.assertEqual(len(node.get_overlay(CertCommunity).certificates), 1, "empty certificates")
+        self.assertEqual(os.path.exists('resource/certificates.txt'), True, "File should still be there")
