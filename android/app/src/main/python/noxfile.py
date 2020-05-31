56b7e1425a282c70a321db4729b9322a9766b4b2
s.cirikka <s.cirikka@student.tudelft.nl>
2020-05-31 02:48:42 +0200

    branch coverage

--- android/app/src/main/python/noxfile.py
+++ android/app/src/main/python/noxfile.py
@@ -6,4 +6,4 @@ def tests(session):
     session.install("pyipv8")
     session.install("asynctest")
     session.install("coverage")
-    session.run('coverage', 'run', '--source=.', '--omit=*.nox/*,noxfile.py,test_*.py', '-m', 'unittest', 'discover')
+    session.run('coverage', 'run', '--branch', '--source=.', '--omit=*.nox/*,noxfile.py,test_*.py', '-m', 'unittest', 'discover')
