a66309234140f5bdcbf7a0422cc07db25e902330
Raul Santana Trejo <r.santanatrejo@student.tudelft.nl>
2020-06-10 17:42:21 +0100

    Changed status bar color, fixed error and changed color in Help Button

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Provider } from "./Store";
 import DrawerMenu from './components/DrawerMenu';
+import { StatusBar } from 'react-native';
 
 //Main entry point for the app runtime.
 
+const changeBar = () =>{StatusBar.setBackgroundColor('dodgerblue')}
+
 export default function App() {
+
+  changeBar()
   { /*The provider allows for props to be automatically passed down the component tree*/ }
   return (
     <Provider>
+
       <DrawerMenu />
     </Provider>
   );
