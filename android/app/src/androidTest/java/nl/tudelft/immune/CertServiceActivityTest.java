eafae70bdb604c214c08916c890a74576dd5b1e2
kkostadinov <k.k.kostadinov@student.tudelft.nl>
2020-06-13 14:32:27 +0200

    Changed test name.

--- android/app/src/androidTest/java/nl/tudelft/immune/CertServiceActivityTest.java
+++ android/app/src/androidTest/java/nl/tudelft/immune/CertServiceActivityTest.java
@@ -21,7 +21,7 @@ import static org.junit.Assert.assertEquals;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class CertServiceWidgetTest {
+public class CertServiceActivityTest {
 
   @Rule
   public final ServiceTestRule serviceRule = new ServiceTestRule();
