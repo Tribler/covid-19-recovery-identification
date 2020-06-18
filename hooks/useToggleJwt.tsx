c42ccf9b8f76d835a35d6ae9c75179ec261ee06d
s.cirikka <s.cirikka@student.tudelft.nl>
2020-06-18 15:49:09 +0200

    extract jwt payload to see if attester or not

--- hooks/useToggleJwt.tsx
+++ hooks/useToggleJwt.tsx
@@ -1,14 +1,18 @@
 import { useCallback } from "react";
 import { useSetDraft } from "../Store";
+import { useToggleAttester } from "./useToggleAttester";
 
 /**
  * Hook used to change the JWT in the store.
  */
 export const useToggleJwt = () => {
     const setDraft = useSetDraft();
+    const updateAttester = useToggleAttester()
+
     return useCallback((jwt: string) => {
         setDraft((draft) => {
             draft.jwt = jwt;
+            updateAttester(jwt)
         });
     }, [setDraft]);
 };
