b9ee7a73d441e64839ebb0a627996d15997ed0f3
kkostadinov <k.k.kostadinov@student.tudelft.nl>
2020-06-19 20:28:46 +0200

    Reverted widget control changes

--- android/app/src/main/java/nl/tudelft/immune/MainApplication.java
+++ android/app/src/main/java/nl/tudelft/immune/MainApplication.java
@@ -21,8 +21,6 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be auto-linked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
           return packages;
         }
 
